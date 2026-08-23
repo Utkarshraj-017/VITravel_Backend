44060ac19bb38e87dc79c75006b29e82fe4d1536
sidpiee <sids23104@gmail.com>
2026-08-23 22:46:00 +0530

    fix: cors error fix

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
-const frontendOrigin = process.env.FRONTEND_URL || "http://localhost:3000";
+const frontendOrigin = "http://localhost:3000" || process.env.FRONTEND_URL ; 
 
 app.use(cors({
     origin: frontendOrigin,
